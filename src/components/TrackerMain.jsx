b66a7787a77d6aec3c474d7d555b6cd2161e06b5
Jagerziel <fromnytosg@gmail.com>
2024-03-29 15:17:35 -0400

    Added dynamic sizing modifier

--- src/components/TrackerMain.jsx
+++ src/components/TrackerMain.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-import './TrackerMain.scss'
+import React, { useState } from "react";
+import "./TrackerMain.scss";
 
-export default function TrackerMain() {
-  return (
-    <div className='trackermain-component'>TrackerMain</div>
-  )
+export default function TrackerMain({ windowMod }) {
+    let fontMod = (windowMod.x + windowMod.y) / 2;
+    return (
+        <div className="trackermain-container">
+        <div
+            className="trackermain-title"
+            style={{
+            fontSize: 60 * fontMod,
+            }}
+        >
+            Authority
+        </div>
+        <div>
+            <input
+            className="inputField"
+            type="number"
+            style={{
+                height: 40 * fontMod,
+                width: '60%',
+                fontSize: 40 * fontMod,
+                border: '2px solid rgb(0, 255, 115)'
+            }}
+            ></input>
+        </div>
+        <div>Authority</div>
+        </div>
+    );
 }
--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // Import Component
 import TrackerMain from '../components/TrackerMain.jsx'
 import TrackerSub from '../components/TrackerSub.jsx'
@@ -7,10 +7,52 @@ import TrackerSub from '../components/TrackerSub.jsx'
 import './Dashboard.scss'
 
 export default function Dashboard() {
+  const [ windowMod, setWindowMod ] = useState({
+    x: 0,
+    y: 0
+  })
+  
+  // Size of Window
+  const [size, setSize] = useState({
+    x: window.innerWidth,
+    y: window.innerHeight
+  })
+  
+  // Update size object when window is resized
+  function updateSize () {
+    setSize({
+      x: window.innerWidth,
+      y: window.innerHeight
+    })
+  }
+  
+  // Sets Window Size
+  useEffect(() => (
+    window.onresize = updateSize
+  ), [])
+  
+  // Sets Modifier
+  useEffect(() => (
+    modifier()
+  ),[size])
+
+  function modifier () {
+    let baseX = 1200
+    let baseY = 800
+    setWindowMod({
+      x: parseInt(window.innerWidth) / baseX,
+      y: parseInt(window.innerHeight) / baseY
+    })
+  }
+      
+  // console.log(size)
+  // console.log(windowMod)
+  
+  
   return (
     <div className='dashboard-container'>
       <div className='dashboard-left-container'>
-        <TrackerMain />
+        <TrackerMain windowMod={windowMod} />
       </div>
       <div className='dashboard-right-container'>
         
