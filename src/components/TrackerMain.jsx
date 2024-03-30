79f70058016c6b29dbd17dae0e575db081d8d74c
Jagerziel <fromnytosg@gmail.com>
2024-03-29 21:35:31 -0400

    Updated TrackerMain box dimensions

--- src/components/TrackerMain.jsx
+++ src/components/TrackerMain.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./TrackerMain.scss";
 
 export default function TrackerMain({ windowMod }) {
-  const [value, setValue] = useState(30);
+  const [value, setValue] = useState(50);
 
   let fontMod = (windowMod.x + windowMod.y) / 2;
 
@@ -28,9 +28,9 @@ export default function TrackerMain({ windowMod }) {
             setValue(newVal);
           }}
           style={{
-            height: 40 * fontMod,
+            height: 100 * fontMod,
             width: "80%",
-            fontSize: 40 * fontMod,
+            fontSize: 100 * fontMod,
             border: "2px solid rgb(0, 255, 115)",
           }}
         />
@@ -39,7 +39,7 @@ export default function TrackerMain({ windowMod }) {
           style={{
             height: 80 * fontMod,
             width: "80%",
-            border: "2px solid rgb(0, 255, 115)",
+            // border: "2px solid rgb(0, 255, 115)",
           }}
         >
           <div
@@ -59,8 +59,40 @@ export default function TrackerMain({ windowMod }) {
               +
             </div>
           </div>
-          <div>B</div>
-          <div>C</div>
+          <div
+            className="trackermain-button"
+            onClick={() => {
+              let newVal = isNaN(parseInt(value)) ? 0 : parseInt(value) - 1;
+              setValue(newVal);
+            }}
+          >
+            <div
+              className="trackermain-button-text"
+              style={{
+                fontSize: 90 * fontMod,
+                marginTop: -10 * fontMod,
+              }}
+            >
+              -
+            </div>
+          </div>
+          <div
+            className="trackermain-button2"
+            onClick={() => {
+              let newVal = 50;
+              setValue(newVal);
+            }}
+          >
+            <div
+              className="trackermain-button-text"
+              style={{
+                fontSize: 20 * (fontMod * 2.5),
+                marginTop: -5 * fontMod,
+              }}
+            >
+              Reset
+            </div>
+          </div>
         </div>
       </div>
       <div style={{ height: 60 * fontMod }}></div>
