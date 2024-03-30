84f45574a71a637b8f5d55d55564e5bafdf8326e
Jagerziel <fromnytosg@gmail.com>
2024-03-29 20:45:11 -0400

    Fixed issue with uncentered text for addition symbol on button

--- src/components/TrackerMain.jsx
+++ src/components/TrackerMain.jsx
@@ -2,30 +2,68 @@ import React, { useState } from "react";
 import "./TrackerMain.scss";
 
 export default function TrackerMain({ windowMod }) {
-    let fontMod = (windowMod.x + windowMod.y) / 2;
-    return (
-        <div className="trackermain-container">
+  const [value, setValue] = useState(30);
+
+  let fontMod = (windowMod.x + windowMod.y) / 2;
+
+  console.log(value);
+
+  return (
+    <div className="trackermain-container">
+      <div
+        className="trackermain-title"
+        style={{
+          fontSize: 60 * fontMod,
+        }}
+      >
+        Authority
+      </div>
+      <div className="trackermain-subcontainer">
+        <input
+          className="inputField"
+          type="number"
+          value={value}
+          onChange={(e) => {
+            let newVal = e.target.value;
+            setValue(newVal);
+          }}
+          style={{
+            height: 40 * fontMod,
+            width: "80%",
+            fontSize: 40 * fontMod,
+            border: "2px solid rgb(0, 255, 115)",
+          }}
+        />
         <div
-            className="trackermain-title"
-            style={{
-            fontSize: 60 * fontMod,
-            }}
+          className="trackermain-button-container"
+          style={{
+            height: 80 * fontMod,
+            width: "80%",
+            border: "2px solid rgb(0, 255, 115)",
+          }}
         >
-            Authority
-        </div>
-        <div>
-            <input
-            className="inputField"
-            type="number"
-            style={{
-                height: 40 * fontMod,
-                width: '60%',
-                fontSize: 40 * fontMod,
-                border: '2px solid rgb(0, 255, 115)'
+          <div
+            className="trackermain-button"
+            onClick={() => {
+              let newVal = isNaN(parseInt(value)) ? 0 : parseInt(value) + 1;
+              setValue(newVal);
             }}
-            ></input>
+          >
+            <div
+              className="trackermain-button-text"
+              style={{
+                fontSize: 80 * fontMod,
+                marginTop: -10 * fontMod,
+              }}
+            >
+              +
+            </div>
+          </div>
+          <div>B</div>
+          <div>C</div>
         </div>
-        <div>Authority</div>
-        </div>
-    );
+      </div>
+      <div style={{ height: 60 * fontMod }}></div>
+    </div>
+  );
 }
